e47fb1f58736fe0b90f1a74bdef3a14b31a883c7
MantuP <pradhanprabina12@gmail.com>
2024-12-10 13:05:34 +0530

    Remove .

--- log_Work/TAPIN/ProcessTAP.sh
+++
@@ -1,2 +0,0 @@
-./TAPIN PM_MTNGH_CLIENT/PM_MTNGH_CLIENT@ORCL 2 1 /home/WBS_4.4/PM/GHANA_TAPIN_LTE_TEST_4.4_CODE/log/  TAPIN 45
-#./ProcessTAP pm_rdm/pm_rdm@lt29 2 1 /home/roaming/roam/roam/PM/RAP_DEVELOPMENT/TAPIN/log/  TAPIN 45
